648833937c1510528b1127c4d86689fe8787738e
g-bulgarit <alonio6@gmail.com>
2022-02-25 09:28:59 +0200

    [PA1] Sender and reciever working

--- PA1_NoisyChannel/Receiver/Receiver.c
+++ PA1_NoisyChannel/Receiver/Receiver.c
@@ -1,6 +1,58 @@
 #include <stdio.h>
 
-int main()
-{
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include "winsock2.h"
+
+#pragma comment(lib, "ws2_32.lib") //Winsock Library
+
+#define DEFAULT_PORT 6003
+#define MSG_SIZE 300
+
+int main() {
+	// Setup recv buffer
+	char recvBuf[2500];
+
+	// Set up connection details
+	struct sockaddr_in server;
+	server.sin_family = AF_INET;
+	server.sin_port = htons(DEFAULT_PORT);
+	server.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	struct sockaddr_in client;
+
+	// initialize windows networking and check for errors.
+	WSADATA wsaData;
+	SOCKET rxSocket;
+
+	int wsaRes = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (wsaRes != NO_ERROR) {
+		printf("[ERR] WSAStartup() failed.\r\n");
+		exit(1);
+	}
+
+	// Create actual IPv4 TCP socket
+	if ((rxSocket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
+	{
+		printf("[ERR] Failed to create socket, error code %d\r\n", WSAGetLastError());
+		exit(1);
+	}
+
+	// Bind socket
+	int retcode = bind(rxSocket, (SOCKADDR*)&server, sizeof(server));
+	int listen_retcode = listen(rxSocket, 1);
+	// TODO: Test retcodes
+
+	printf("[Start] Started socket for Reciever\r\n");
+
+	while (1) {
+		int c = sizeof(struct sockaddr_in);
+		SOCKET s = accept(rxSocket, (SOCKADDR*)&client, &c);
+		int recv_bytes = recv(s, recvBuf, MSG_SIZE, 0);
+		if (recv_bytes) {
+			printf("[Success] Recieved %d bytes\r\n", recv_bytes);
+			printf("[Success] Recieved: %s\r\n", recvBuf);
+		}
+	}
+
 	return 0;
 }
--- PA1_NoisyChannel/Sender/Sender.c
+++ PA1_NoisyChannel/Sender/Sender.c
@@ -1,6 +1,45 @@
 #include <stdio.h>
 
-int main()
-{
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include "winsock2.h"
+
+#pragma comment(lib, "ws2_32.lib") //Winsock Library
+
+#define DEFAULT_PORT 6003
+
+int main() {
+
+	// Set up connection details
+	struct sockaddr_in remote;
+	remote.sin_family = AF_INET;
+	remote.sin_port = htons(DEFAULT_PORT);
+	remote.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+
+	// initialize windows networking and check for errors.
+	WSADATA wsaData;
+	SOCKET txSocket;
+
+	int wsaRes = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (wsaRes != NO_ERROR) {
+		printf("[ERR] WSAStartup() failed.\n");
+		exit(1);
+	}
+
+	// Create actual IPv4 TCP socket
+	if ((txSocket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
+	{
+		printf("[ERR] Failed to create socket, error code %d", WSAGetLastError());
+		exit(1);
+	}
+
+	// Connect to server socket
+	int listen_retcode = connect(txSocket, (SOCKADDR*)&remote, sizeof(remote));
+	printf("[Start] Connected to Reciever\n");
+
+	char msg[] = "Fuck tel aviv university";
+	int sent_bytes = send(txSocket, msg, sizeof(msg), 0);
+	printf("[Success] Sent %d bytes\n", sent_bytes);
+
 	return 0;
 }
