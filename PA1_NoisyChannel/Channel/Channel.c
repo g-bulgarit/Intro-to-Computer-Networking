11d403066a1679e93d9600307f91c4711c495ad1
Alon Budker <alonbudker@mail.tau.ac.il>
2022-03-14 21:13:39 +0200

    [PA1] Refactor: Moved sockets to function, slight doc update

--- PA1_NoisyChannel/Channel/Channel.c
+++ PA1_NoisyChannel/Channel/Channel.c
@@ -1,26 +1,32 @@
+// General Imports
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
+// Winsock Imports
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include "winsock2.h"
 #pragma comment(lib, "ws2_32.lib") //Winsock Library
 
+// Constants and other stuff
 #define LISTEN_PORT		6000
 #define SEND_PORT		6001
 #define MSG_SIZE		500000
 #define BYTE_SIZE_IN_BITS 8
 #define DEBUG
 
+#define LISTEN			1
+#define SEND			2
+
 
 void flipBit(char* buffer, int index) {
 	// Flip specific bit in buffer
-
 	buffer[index / BYTE_SIZE_IN_BITS] ^= (1u << (7 - (index % BYTE_SIZE_IN_BITS)));
-
 }
 
 void addDeterministicNoise(int n, char* buffer, int bufSize, int* flippedBits) {
+	// Flip every n-th bit in the message.
 	for (int bitIdx = 0; bitIdx < bufSize * BYTE_SIZE_IN_BITS; bitIdx += n)
 	{
 		flipBit(buffer, bitIdx);
@@ -38,9 +44,39 @@ void addRandomNoise(int prob, int seed, char* buffer, int bufferSize, int* flipp
 			flipBit(buffer, i);
 			*flippedBits += 1;
 		}
-		
+	}
+}
+
+SOCKET createSocket(char* ipAddress, int port, int mode, struct sockaddr_in* sockStruct) {
+	// Function to construct and return a working socket in either 'listen' or 'send' mode.
+	SOCKET s;
+	WSADATA wsaData;
+
+	sockStruct->sin_family = AF_INET;
+	sockStruct->sin_port = htons(port);
+	sockStruct->sin_addr.s_addr = inet_addr(ipAddress);
+
+	// Init winsockets
+	int wsaRes = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (wsaRes != NO_ERROR) {
+		printf("[ERR] WSAStartup() failed.\r\n");
+		exit(1);
 	}
 
+	// Create actual IPv4 TCP socket for listening
+	if ((s = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
+	{
+		printf("[ERR] Failed to create listening socket, error code %d\r\n", WSAGetLastError());
+		exit(1);
+	}
+
+	if (mode == LISTEN) {
+		// Bind listen socket
+		int retcode = bind(s, (SOCKADDR*)sockStruct, sizeof(*sockStruct));
+		int canListen = listen(s, 1);
+	}
+
+	return s;
 }
 
 int main(int argc, char* argv[]) {
@@ -48,13 +84,18 @@ int main(int argc, char* argv[]) {
 	char* flag = "";
 	int noiseAmt = 0;
 	int randomNoiseSeed = 0;
+
 	// User input
 	char userInputBuffer[5];
 	int amtBitsFlipped = 0;
 
+	// recv buffer
+	char recvBuf[MSG_SIZE];
 
+	// Basic user greeting and helptext
 	printf("-------[NOISY CHANNEL]------- \r\n\r\n");
 
+	// Deal with argv
 	if (argc <= 2) {
 		printf("[INFO] A noise function flag was not provided.\r\n\tDefaulting to no noise in transmission!.\r\n\t");
 		printf("To add random noise, use the -r <probability> <seed>, and for deterministic noise use -d <amount>.\r\n");
@@ -66,45 +107,15 @@ int main(int argc, char* argv[]) {
 			randomNoiseSeed = atoi(argv[3]);
 	}
 
-
-	// Setup recv buffer
-	char recvBuf[MSG_SIZE];
-
-	// Set up connection detail structs
-	struct sockaddr_in sListen;
-	sListen.sin_family = AF_INET;
-	sListen.sin_port = htons(LISTEN_PORT);
-	sListen.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-	struct sockaddr_in sSend;
-	sSend.sin_family = AF_INET;
-	sSend.sin_port = htons(SEND_PORT);
-	sSend.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-	// initialize windows networking and check for errors.
-	WSADATA wsaData;
+	// Initialize sockets :)
+	struct sockaddr_in sListen, sSend;
 	SOCKET listenSocket;
 	SOCKET transmitSocket;
-
-	int wsaRes = WSAStartup(MAKEWORD(2, 2), &wsaData);
-	if (wsaRes != NO_ERROR) {
-		printf("[ERR] WSAStartup() failed.\r\n");
-		exit(1);
-	}
-
-	// Create actual IPv4 TCP socket for listening
-	if ((listenSocket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
-	{
-		printf("[ERR] Failed to create listening socket, error code %d\r\n", WSAGetLastError());
-		exit(1);
-	}
-
-	// Bind listen socket
-	int retcode = bind(listenSocket, (SOCKADDR*)&sListen, sizeof(sListen));
-	int canListen = listen(listenSocket, 1);
-		// TODO: Test retcodes here
+	listenSocket = createSocket("127.0.0.1", LISTEN_PORT, LISTEN, &sListen);
+	transmitSocket = createSocket("127.0.0.1", SEND_PORT, SEND, &sSend);
 
 	while (1) {
+		// Main action loop
 
 		printf("[Start] Started listening to the recieve socket, waiting for sender\r\n");
 	#ifdef DEBUG
@@ -141,13 +152,13 @@ int main(int argc, char* argv[]) {
 			printf("\r\nAdded random noise!, flipped %d bits\r\n", amtBitsFlipped);
 		}
 
-
 		// Create actual IPv4 TCP socket for sending
 		if ((transmitSocket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
 		{
 			printf("[ERR] Failed to create transmitting socket, error code %d\r\n", WSAGetLastError());
 			exit(1);
 		}
+
 		// Connect to server socket
 		int listen_retcode = connect(transmitSocket, (SOCKADDR*)&sSend, sizeof(sSend));
 		printf("[Success] Connected to reciever \r\n");
@@ -161,7 +172,6 @@ int main(int argc, char* argv[]) {
 		if (!strcmp(userInputBuffer, "no")) {
 			exit(0);
 		}
-
 	}
 
 	return 0;
