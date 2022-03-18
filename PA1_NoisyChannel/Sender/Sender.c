e888b2241ee515f8823841b13d940bb9b185f52a
Alon Budker <alonbudker@mail.tau.ac.il>
2022-03-18 11:40:27 +0200

    [PA1] Sender: use IP from cmdline args as target

--- PA1_NoisyChannel/Sender/Sender.c
+++ PA1_NoisyChannel/Sender/Sender.c
@@ -28,7 +28,7 @@ int main(int argc, char* argv[]) {
 	
 	while (!stopUserInput) {
 		// Connect to server socket
-		txSocket = createSocket("127.0.0.1", port, SEND, &remote);
+		txSocket = createSocket(argv[1], port, SEND, &remote);
 		int listen_retcode = connect(txSocket, (SOCKADDR*)&remote, sizeof(remote));
 
 #ifdef DEBUG
