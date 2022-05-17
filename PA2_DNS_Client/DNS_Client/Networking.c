d8af07316db7a60dc15b0084b86d01b329315995
Alon Budker <alonbudker@mail.tau.ac.il>
2022-05-17 21:57:39 +0300

    [PA2] Networking base code

---
+++ PA2_DNS_Client/DNS_Client/Networking.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include "winsock2.h"
+#pragma comment(lib, "ws2_32.lib") //Winsock Library
+#include <Ws2tcpip.h>
+
+#include "Networking.h"
+
+// Constants and general use variables
+int wsaInitialized = 0;
+WSADATA wsaData;
+SOCKET sendSocket;
+
+struct sockaddr_in netSocket;
+int netSocketSize = sizeof(netSocket);
+
+char recvBuffer[BUFFER_SIZE];
+unsigned int recvBytes = 0;
+
+
+void initUDP(char *serverIpAddr, unsigned int serverPort)
+{
+	int retval;
+	// Try to init the winsocket thing
+	if (!wsaInitialized)
+	{
+		retval = WSAStartup(MAKEWORD(2, 2), &wsaData);
+		if (retval != 0)
+			// TODO: Print error here
+			exit(1);
+		wsaInitialized = 1;
+	}
+
+	// Create our socket structure
+	memset((void *)&netSocket, 0, sizeof(netSocket));
+	netSocket.sin_family = AF_INET;
+	netSocket.sin_addr.s_addr = inet_addr(serverIpAddr);
+	netSocket.sin_port = htons(serverPort);
+
+	// Create actual socket
+	if ((sendSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
+	{
+		perror("error: unable to initialize socket");
+		return;
+	}
+
+	// Connect to the DNS server specified
+	if (connect(sendSocket, (const struct sockaddr *)&netSocket, sizeof(netSocket)) != 0)
+	{
+		return;
+	}
+
+}
+
+
+int sendUDP(char *buf, int bufferLength)
+{
+	int resp = sendto(sendSocket, buf, bufferLength, 0, (struct sockaddr*)&netSocket, sizeof(netSocket));
+	return resp;
+}
+
+char* receiveUDP(int *recvBufferLength)
+{
+	int recvBits = recvfrom(sendSocket, recvBuffer, BUFFER_SIZE, 0, (struct sockaddr*) &netSocket, &netSocketSize);
+	*recvBufferLength = recvBits;
+	return recvBuffer;
+}
+
+void closeUDPSocket()
+{
+	closesocket(sendSocket);
+}
---
+++ PA2_DNS_Client/DNS_Client/Networking.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define BUFFER_SIZE  1024
+#define TIMEOUT_SECONDS  2
